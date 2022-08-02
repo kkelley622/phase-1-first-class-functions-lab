189c96d4d7be94a727f3cd2fd2646804fd94e353
Kevin Kelley <kkelley622@gmail.com>
2022-08-02 10:36:57 -0500

    commit tree 0c84a0cf031daf3366b54516502a5324e070be1b

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = (arrayOfDrivers) => {
+    return arrayOfDrivers.slice(0,2);
+    
+}
+
+// return last two drivers
+
+const returnLastTwoDrivers = (arrayOfDrivers) => {
+    return arrayOfDrivers.slice(- 2);
+}
+
+// "selecting drivers" array with previous functions as elements
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// fare multiplier that returns a function that multiplies an integer by 4
+
+const createFareMultiplier = (integer) => {
+    return function (multiplyValue) {
+        return integer * multiplyValue
+    }
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+// return the first two driver when returnFirstTwoDrivers() is passed as the second argument
+
+// return the last two drivers when passd returnLastTwoDrivers() as the second argument
+
+function selectDifferentDrivers(arrayOfDrivers, returnedDrivers) {
+    return returnedDrivers(arrayOfDrivers)
+}
+
+
